3b6680e02096908201d768df94f24a1f9015ebb6
Mateus <mateusjose44@hotmail.com>
2020-01-17 15:06:05 -0300

    realizando novo commit de teste

--- SerieGit/SerieGit/Program.cs
+++ SerieGit/SerieGit/Program.cs
@@ -3,7 +3,10 @@
 namespace SerieGit {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("isso é um teste!!!!");
+
+
+
         }
     }
 }
